ffcd9b97d0f9499c5bf63f42539cdee6428e4f2d
Anna <anna_d1@ukr.net>
2024-11-15 19:20:18 +0200

    first commit

---
+++ JS2/file.js
@@ -0,0 +1,8 @@
+
+
+        //console.log("Hello 1!");
+        console.log("Hello 2!");
+        console.log("Hello 3!");
+        // console.log("Hello 4!");
+        // console.log("Hello 5!");
+  
